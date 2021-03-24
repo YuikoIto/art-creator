ef2624b2472dbb12bbf5d0451c8a2bb48bb13a13
YuikoIto <toffywing@gmail.com>
2021-03-24 22:56:49 +0900

    feat::tada:change spa to ssr && import modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,25 +1,26 @@
+require("dotenv").config();
+const { API_BASE_URL, API_OGP_URL } = process.env;
 export default {
-  mode: 'spa',
   /*
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || '',
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || "",
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: { color: "#fff" },
   /*
    ** Global CSS
    */
@@ -28,6 +29,13 @@ export default {
    ** Plugins to load before mounting the App
    */
   plugins: [],
+
+  components: true,
+
+  env: {
+    API_BASE_URL,
+    API_OGP_URL,
+  },
   /*
    ** Nuxt.js modules
    */
